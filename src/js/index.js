f673548375b36a51265c8dab4180ed42f7309cce
emirevec <vecchioemiliano@gmail.com>
2023-12-31 17:21:58 -0300

    refactor(web): move form listeners into render function

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,38 @@
 let data = [];
 
-function render (url, id) {
+function render(url, id) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.send();
     xhr.addEventListener('load', () => {
         const template = Handlebars.compile(xhr.response);
         $("#" + id).empty();
-        $("#" + id).append(template({data}));
+        $("#" + id).append(template({ data }));
+        $("#search_order").on("change", (e) => {
+            switch (e.target.value) {
+                case 'mayorPrecio':
+                    data = data.sort((a, b) =>
+                        Number(a.precio) < Number(b.precio) ? 1 : -1
+                    )
+                    break;
+                case 'menorPrecio':
+                    data = data.sort((a, b) =>
+                        Number(a.precio) > Number(b.precio) ? 1 : -1
+                    )
+                    break;
+            };
+            render('../src/views/app.hbs', "app");
+        });
+        $("#search_submit").on("click", (e) => {
+            e.preventDefault();
+            const searchName = $("#search_name").val().toLocaleLowerCase();
+            data = data.filter((item) => item.nombre.toLocaleLowerCase() == searchName);
+            render('../src/views/app.hbs', "app");
+        });
     });
 };
 
-function getData () {
+function getData() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '../src/data/products.json')
     xhr.send();
@@ -21,30 +42,8 @@ function getData () {
     });
 };
 
-$("#search_order").on("change", (e) => {
-    switch (e.target.value) {
-        case 'mayorPrecio':
-            data = data.sort((a, b) =>
-                Number(a.precio) < Number(b.precio) ? 1 : -1
-            )
-            break;
-        case 'menorPrecio':
-            data = data.sort((a, b) =>
-                Number(a.precio) > Number(b.precio) ? 1 : -1
-            )
-            break;
-    };
-    render('../src/views/app.hbs', "app");
-});
 
-$("#search_submit").on("click", (e) => {
-    e.preventDefault();
-    const searchName = $("#search_name").val().toLocaleLowerCase();
-    data = data.filter((item) => item.nombre.toLocaleLowerCase() == searchName);
-    render('../src/views/app.hbs', "app");
-});
-
-$(function(){
+$(function () {
     render('../src/views/app.hbs', "app");
     getData();
 });
