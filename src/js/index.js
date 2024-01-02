de590caade1a833b36c33402c9fd2accf3a566ae
emirevec <vecchioemiliano@gmail.com>
2024-01-02 12:05:28 -0300

    perf(web): extract getData function

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import getData from "./getData.js";
+
 let data = [];
 
 function renderApp() {
@@ -8,50 +10,37 @@ function renderApp() {
         const template = Handlebars.compile(xhr.response);
         $("#app").empty();
         $("#app").append(template({ data }));
+        
+        
         $("#search").on("submit", (e) => {
             e.preventDefault();
+                        
             const searchName = $("#search_name").val().toLocaleLowerCase();
             const order = $("#search_order").val();
 
             if (searchName) {
                 data = data.filter((item) => item.nombre.toLocaleLowerCase() == searchName);
             }
-
+            
             switch (order) {
                 case 'mayorPrecio':
                     data = data.sort((a, b) =>
-                        Number(a.precio) < Number(b.precio) ? 1 : -1
+                    Number(a.precio) < Number(b.precio) ? 1 : -1
                     )
                     break;
-                case 'menorPrecio':
-                    data = data.sort((a, b) =>
+                    case 'menorPrecio':
+                        data = data.sort((a, b) =>
                         Number(a.precio) > Number(b.precio) ? 1 : -1
-                    )
-                    break;
-            };
+                        )
+                        break;
+                    };
 
             renderApp();
         });
     });
 };
-
-function getData() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '../src/data/products.json')
-    xhr.send();
-    xhr.addEventListener('load', () => {
-        data = JSON.parse(xhr.response);
-        renderApp();
-    });
-};
-
-function getParams(){
-    const params = new URLSearchParams(location);
-    console.log(params);
-};
+//const params = new URLSearchParams(location);
 
 $(function () {
-    renderApp();
-    getData();
-    getParams();
+    getData(renderApp);
 });
