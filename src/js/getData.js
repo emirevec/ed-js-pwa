458b125507d3f95b465e7b5814db0a124db08abf
emirevec <vecchioemiliano@gmail.com>
2024-01-12 15:54:54 -0300

    feat(web): add getCachedData.js to set 'data' in localStorage

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,5 +1,7 @@
+import getCachedData from "./getCachedData.js";
+
 export async function getData(url){
-    const response = await fetch(url);
-    let data = await response.json();
+    const response = await getCachedData(url);
+    let data = await JSON.parse(response);
     return data;
 };
